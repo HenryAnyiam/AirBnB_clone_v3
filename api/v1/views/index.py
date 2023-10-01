c0de4567787fcb3f8757217c8ff4bb707d848e63
Henry Anyiam <anyiamhenry7@gmail.com>
2023-09-30 21:31:48 -0700

    added stats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,29 @@
 
 from flask import jsonify
 from api.v1.views import app_views
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route("/status")
 def index():
     """returns a 200 JSON status"""
-    return jsonify({"starus": "OK"})
+    return jsonify({"status": "OK"})
+
+
+@app_views.route("/stats")
+def stats():
+    stat = {
+            "amenities": storage.count(Amenity),
+            "cities": storage.count(City),
+            "places": storage.count(Place),
+            "reviews": storage.count(Review),
+            "states": storage.count(State),
+            "users": storage.count(User)
+            }
+    return jsonify(stat)
